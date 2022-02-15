827e6140b4a14f39fdc2e00153b04b6aac5dcd38
Kate Shauliuk <Katsiarynabaranava@gmail.com>
2022-02-15 20:08:31 +0300

    Updated CheckOut Page and CheckOutTest.

--- src/test/java/pages/CheckOutPage.java
+++ src/test/java/pages/CheckOutPage.java
@@ -6,8 +6,6 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.ui.Select;
 
-import static tests.BaseTest.*;
-
 
 public class CheckOutPage extends BasePage {
 
